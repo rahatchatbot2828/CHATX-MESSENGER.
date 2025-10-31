c338c92de4e6e14b3fca4b60c2611b3a2ec11354
goat-bot-azad <talukdershoikat@gmail.com>
2025-10-31 12:21:20 +0600

    Update backupdata.js

--- scripts/cmds/backupdata.js
+++ scripts/cmds/backupdata.js
@@ -6,7 +6,7 @@ module.exports = {
 		version: "1.3",
 		author: "NTKhang",
 		countDown: 5,
-		role: 2,
+		role: 3,
 		description: {
 			vi: "Sao lưu dữ liệu của bot (threads, users, dashboard, globalData)",
 			en: "Backup data of bot (threads, users, dashboard, globalData)"
